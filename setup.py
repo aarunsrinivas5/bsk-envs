2aad359cee92405eb2bf905f607799fc62714851
aarunsrinivas5 <aarun5@umd.edu>
2025-03-08 23:33:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='bsk-envs',
     version='0.0.2',
-    keywords='basilisk, hohmann, interplanetary, transfers',
-    url='https://github.com/aarunsrinivas5/bsk-transfers.git',
-    description='Gymnasium environments built on top of Basilisk for simulating transfers',
+    keywords='basilisk, hohmann, interplanetary, orbit discovery, transfers',
+    url='https://github.com/aarunsrinivas5/bsk-envs.git',
+    description='Gymnasium environments built on top of Basilisk',
     packages=find_packages(),
     install_requires=[
         'numpy',
